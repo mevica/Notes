0838ad60e7fdabbc6ec0908785ca43a4d61f4ede
mevica <mevicandanu@gmail.com>
2020-08-01 15:03:53 +0300

    create notes and read

---
+++ app/src/main/java/com/example/notes/ListActivity.java
@@ -0,0 +1,58 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+
+        initializeDisplayContent();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged();
+    }
+
+    private void initializeDisplayContent() {
+        final ListView listNotes = findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mAdapterNotes = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(mAdapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(MainActivity.NOTE_INFO, String.valueOf(note));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
